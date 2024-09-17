7f67bf7d53c98dd57422720f0d36389ca7dc9e49
mrchl55 <37666744+mrchl55@users.noreply.github.com>
2024-09-17 17:52:29 +0200

    initial setup

---
+++ src/components/DynamicInput.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const tagsList: string[] = ['React', 'Next.js', 'Tailwind', 'JavaScript', 'CSS'];
+
+interface Tag {
+  id: number;
+  text: string;
+}
+
+export default function DynamicInput() {
+return <></>
+}
